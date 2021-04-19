c9f5f1298a5df0f3e812984651a8223b44aab131
Angie <angie@Joses-MacBook-Pro.local>
2021-04-18 21:16:13 -0300

    agrego nav

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="topBar">
+        <p className='logo'>HALU HANDMADE</p>
+        <nav className='navigation'>
+          <ul className='links'>
+            <li className='link'> 
+              <a href=''>Inicio</a>
+            </li>
+            <li className='link'>
+              <a href=''>Productos</a>
+            </li>
+            <li className='link'>
+              <a href=''>Contacto</a>
+            </li>
+            <li className='link'>
+              <a href=''>Novedades</a>
+            </li>
+          </ul>
+        </nav>
+
       </header>
     </div>
   );
